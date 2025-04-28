8ec2ad4c34ed51874f4196c5cf66f209fc016424
Myo Thant <myothant.thedev@gmail.com>
2025-04-28 20:25:30 +0700

    feat(setup): setup for ez_route

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ez_route",
+    version="0.1.0",
+    description="A lightweight routing system for GUI applications (PyQt, Tkinter, etc.)",
+    author="Myo Thant",
+    author_email="myothant.thedev@gmail.com",
+    url="https://github.com/MyothantTheDev/ez_route",
+    packages = find_packages(where="src"),
+    package_dir = {"": "src"},
+    install_requires = [],
+    extras_require = {
+        "qt": ["PyQt6"],     # if users want Qt support
+        "tk": ["tk"],        # Tkinter is standard
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
