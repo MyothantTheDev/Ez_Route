099271e51c338c76609c2bfea2de7d547bfeb16e
Myo Thant <myothant.thedev@gmail.com>
2025-04-29 23:36:34 +0700

    fix-bug(platform): fix tkinkter frame cannot create properly by adding .place method

--- src/ez_route/platform/screen_factory.py
+++ src/ez_route/platform/screen_factory.py
@@ -5,32 +5,28 @@ from tkinter.ttk import Frame
 class BaseScreenFactory(ABC):
 
   @abstractmethod
-  def create(self, screen: QWidget | Frame, **params):
+  def create(cls, screen: QWidget | Frame, **params):
     pass
 
 class QtScreenFactory(BaseScreenFactory):
 
-  instances: dict[str, QWidget] = {}
-
-  def create(self, screen: QWidget, **params):
-    if screen not in self.instances:
-      self.instances[screen] = screen()
-    if hasattr(self.instances[screen], "with_param") and params:
-      return self.instances[screen].with_param(**params)
-    return self.instances[screen]
+  def create(cls, screen: QWidget, **params):
+    instance = screen()
+    if hasattr(instance, "with_param") and params:
+      instance.with_param(**params)
+    return instance
   
 
 class TkScreenFactory(BaseScreenFactory):
 
   main_frame: Frame = None
-  instances: dict[str, Frame] = {}
 
-  def create(self, screen: Frame, **params) -> Frame:
-    if screen not in self.instances:
-      self.instances[screen] = screen(self.main_frame)
-    if hasattr(self.instances[screen], "with_param") and params:
-      return self.instances[screen].with_param(**params)
-    return self.instances[screen]
+  def create(cls, screen: Frame, **params) -> Frame:
+    instance :Frame = screen(cls.main_frame)
+    instance.place(relx=0, rely=0, relheight=1, relwidth=1)
+    if hasattr(instance, "with_param") and params:
+      instance.with_param(**params)
+    return instance
   
-  def set_mainFrame(self, main_frame: Frame):
-    self.main_frame = main_frame
+  def set_mainFrame(cls, main_frame: Frame):
+    cls.main_frame = main_frame
