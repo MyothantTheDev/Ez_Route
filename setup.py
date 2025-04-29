7f29345357e210bfdb20048d3cb2a5cdb3e6508b
Myo Thant <myothant.thedev@gmail.com>
2025-04-30 00:00:54 +0700

    feat(setup): add description in setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+  description = f.read()
+
 setup(
     name="ez_route",
     version="0.1.0",
@@ -20,4 +23,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
+    long_description=description,
+    long_description_content_type='text/markdown'
 )
